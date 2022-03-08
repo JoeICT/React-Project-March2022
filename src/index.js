94a88b80cc17371498ded45ce88914a0b9022b72
Joseph Hunter <jrhunt1706@gmail.com>
2022-03-08 13:48:43 -0600

    Add string to index.js h1 element

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import './index.css';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
- React.createElement("h1", {style: {color: "blue", textAlign: "center"}}, "Hello!"),
+ React.createElement("h1", {style: {color: "blue", textAlign: "center"}}, "Hello!", " ", "This is Joes React Page"),
 
   document.getElementById('root')
 );
-
 
 // reportWebVitals();
