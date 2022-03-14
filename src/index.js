47d376567992fa804e83f59674087e839a576006
Joseph Hunter <jrhunt1706@gmail.com>
2022-03-13 21:25:57 -0500

    Change h1 color to brown and added font family san serf

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
- React.createElement("h1", {style: {color: "blue", textAlign: "center", backgroundColor: "black"}}, "Hello!", " ", "This is Joes React Page", " ", ", Boring I know..."),
+ React.createElement("h1", {style: {color: "brown", textAlign: "center", backgroundColor: "black", fontFamily: "san serf"}}, "Hello!", " ", "This is Joes React Page", " ", ", Boring I know..."),
 
   document.getElementById('root')
 
