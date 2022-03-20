93dba3cbefd2d36d2c49a6ee53022f55042a1326
Joseph Hunter <jrhunt1706@gmail.com>
2022-03-20 08:37:31 -0500

    Change h3 to h2 on index.js header

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import './index.css';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
- React.createElement("h3", {style: {color: "brown", textAlign: "center", backgroundColor: "black", fontFamily: "'Open Sans', sans-serif"}}, "Hello!", " ", "This is Joes React Page", " ", ", Boring I know..."),
+ React.createElement("h2", {style: {color: "brown", textAlign: "center", backgroundColor: "black", fontFamily: "'Open Sans', sans-serif"}}, "Hello!", " ", "This is Joes React Page", " ", ", Boring I know..."),
 
   document.getElementById('root')
 
 
 );
+
 
 // reportWebVitals();
